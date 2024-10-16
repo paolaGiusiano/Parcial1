bd8266f63cc818e89810b8565403146cdb807f70
unknown <paola.giusiano@correo.ucu.edu.uy>
2024-10-16 11:02:47 -0300

    .

--- Front/src/pages/Receta.jsx
+++ Front/src/pages/Receta.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import RecetasCard from '../components/RecetasCard';
+import './RecetaStyle.css';
 
 
 const Receta = () => {
