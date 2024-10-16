d34a9a68afa142bb7bc2070c11c9644328df414f
unknown <paola.giusiano@correo.ucu.edu.uy>
2024-10-15 23:27:14 -0300

    creación repo

---
+++ Front/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+// import Home  from './pages/Home'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <Routes>
+      
+        {/*<Route path="/*" element={<Navigate replace to="/home" />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/add-sport" element={<AddSport />} />
+
+        <Route path="/sports/:id" element={<Detalles />}  /> 
+        */}
+      </Routes>
+    </>
+  )
+}
+
+export default App
--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Parcial1
+# Parcial 1
+
+
+## Configuración del Frontend
+
+
+1. **Instalar dependencias necesarias:**
+
+   ```bash
+   npm install
+   npm install react-router-dom
+   npm install bulma
+
+2. **Iniciar el servidor:**
+
+     ```bash
+    npm run dev
+
+
+## Configuración del Backend
+
+1. **Instalar dependencias:**
+
+   ```bash
+   npm install express
+
+2. **Iniciar la app:**
+
+     ```bash
+    node app.js
+
